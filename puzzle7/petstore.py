5fdb9f773500a39768d9d723c65a3390dc593df4
Emily Bache <emily@bacheconsulting.com>
2020-11-24 14:24:16 +0000

    get the swagger from the internet with requests which is simpler

--- puzzle7/petstore.py
+++ puzzle7/petstore.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 import json
-from urllib.request import urlopen
+import requests
 
 
 def main():
-    try:
-        url = "http://petstore.swagger.io/v2/swagger.json"
-        response = urlopen(url)
-        swagger = response.read().decode("utf-8")
-        print(json.dumps(swagger))
-    except Exception as e:
-        import traceback, sys
-        traceback.print_exc(file=sys.stdout)
+    response = requests.get("https://petstore.swagger.io/v2/swagger.json")
+    swagger = response.json()
+    print(json.dumps(swagger))
+
 
 if __name__ == "__main__":
     main()
