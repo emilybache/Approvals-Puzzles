1ff66a51c16a547fdd08ab016d75a1a4c4d02968
Emily Bache <emily@bacheconsulting.com>
2020-12-01 07:23:24 +0000

    make normal case more verbose

--- puzzle7/petstore.sh
+++ puzzle7/petstore.sh
@@ -3,6 +3,7 @@
 VERSION=`curl 'https://petstore.swagger.io/v2/swagger.json' | jq '.swagger'`
 echo $VERSION
 if [ "$VERSION" = '"2.0"' ]; then
+  echo "Swagger version is fine"
   exit 0
 fi
 echo "Swagger version is incorrect"
