017736acd8126daed2c47bd5a3b4d41248c7195b
Emily Bache <emily@bacheconsulting.com>
2021-09-10 06:21:18 +0000

    reset to starting position

--- puzzle10/config.ex
+++ puzzle10/config.ex
@@ -7,6 +7,3 @@ filename_convention_scheme:standard
 # Expanded name to use for application
 full_name:example
 
-[run_dependent_text]
-stdout:^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}{REPLACE timestamp}
-stdout:(process_id)=\d+{REPLACE \1=1234}
